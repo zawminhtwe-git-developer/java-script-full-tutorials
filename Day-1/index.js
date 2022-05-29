877c81bc42e5130092ed2ad485749e317bb53d6c
zawminhtwe-git-developer <gitdeveloper2@gmail.com>
2022-05-29 22:04:15 +0630

    Declaration, Varibale, String, Concatenate +,innerHTML

--- Day-1/index.js
+++ Day-1/index.js
@@ -1,10 +1,33 @@
-console.log("My name is Zaw Min Htwe");
-console.log("My name is Zaw Min Htwe");
-window.alert("this is alert");
-window.alert('One Single Code');
+// console.log("My name is Zaw Min Htwe");
+// console.log("My name is Zaw Min Htwe");
+// window.alert("this is alert");
+// window.alert('One Single Code');
 
 // single line comment
 
 /*
 This is multiline comment line 
 */
+
+
+//Two steps 
+// 
+// ကြေညာခြင်းမှာ သုံးမျိုးရှီပါသည်။ declaration (var, let ,const)
+// မှတ်တမ်းတင်ခြင်း assignment (= assignment operator)
+let age; //undefined
+let firstName = "Zaw Min " //stirng
+let lastName = "Htwe"
+age = 21; //integer (or) number
+let student = true //boolean
+
+age = age + 1
+
+
+console.log("Hello ", firstName);
+console.log(firstName + lastName);
+console.log(student);
+document.getElementById("p1").innerHTML = age;
+
+// string နဲ့ variable ပေါ င်း ရေး ချ င် ရင် + sign ကို သုံး ပါ တယ်
+document.getElementById("p2").innerHTML = "You are " + age + " ÿear old";
+document.getElementById("p3").innerHTML = "Hello " + firstName + "...";
