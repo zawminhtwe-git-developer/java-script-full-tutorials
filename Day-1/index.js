27fe323892f278eb21cc57ac50cfa46c5b07f943
zawminhtwe-git-developer <gitdeveloper2@gmail.com>
2022-05-29 16:26:28 +0630

    How to join js file src,console,window.alert,single line comment,multiline comment

---
+++ Day-1/index.js
@@ -0,0 +1,10 @@
+console.log("My name is Zaw Min Htwe");
+console.log("My name is Zaw Min Htwe");
+window.alert("this is alert");
+window.alert('One Single Code');
+
+// single line comment
+
+/*
+This is multiline comment line 
+*/
